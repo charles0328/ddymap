543a772e622bf04c28d09521e2f79a7c3cab99d3
charles0328 <61176613+charles0328@users.noreply.github.com>
2023-02-02 22:54:44 +0900

    Revert "부트스트랩/폰트(Notosan) 적용, 페이지(index,board,about)/헤더/로고 추가"
    This reverts commit 812f8e4fb67c825c50e29b9a045d873e586a22e3.

--- app.py
+++ app.py
@@ -5,14 +5,6 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
-@app.route('/board')
-def board():
-   return render_template('board.html')
-
-@app.route('/about')
-def about():
-   return render_template('about.html')
-
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
 
