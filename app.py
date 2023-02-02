e0d4eac406f44549523136cce64491b037b2979e
charles0328 <61176613+charles0328@users.noreply.github.com>
2023-02-02 23:04:33 +0900

    부트스트랩/폰트(Notosan) 적용, 페이지(index,board,about)/헤더/로고 추가

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
+@app.route('/board')
+def board():
+   return render_template('board.html')
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
 
