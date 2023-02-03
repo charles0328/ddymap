b6d89b8b4e736c9a6fb2db5aef02bc75cf219b38
charles0328 <61176613+charles0328@users.noreply.github.com>
2023-02-03 11:18:37 +0900

    mongodb 연동

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://charles:1111@cluster0.szvramp.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbddymap
+
 @app.route('/')
 def home():
    return render_template('index.html')
@@ -15,4 +22,3 @@ def about():
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
-
