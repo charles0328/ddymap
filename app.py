601d7ed6f4d082de0156dd31040a5f7cb085e0cc
yunzin <hanyj7650@gmail.com>
2023-02-08 21:34:08 +0900

    게시판-비밀번호입력 후 삭제

--- app.py
+++ app.py
@@ -16,12 +16,14 @@ def home():
     return render_template('index.html')
 
 
+# 게시판 페이지
 @app.route('/board')
 def board():
     count = db.board.count_documents({})
     return render_template('board.html', count=count)
 
 
+# 게시물 DB 등록
 @app.route('/board/regist', methods=["POST"])
 def board_post():
     nickname_receive = request.form['nickname_give']
@@ -41,27 +43,54 @@ def board_post():
         'title': title_receive,
         'content': content_receive,
         'url': url_receive,
-        'radio': radio_receive
+        'radio': radio_receive,
     }
     db.board.insert_one(doc)
 
     return jsonify({'msg': '게시물 등록 완료!'})
 
 
+# 게시물 게시
 @app.route('/board/regist', methods=["GET"])
 def board_get():
     cards_list = list(db.board.find({}, {'_id': False}))
     return jsonify({'cards': cards_list})
 
-@app.route('/board/regist/delete', methods=["POST"])
-def card_delete():
-    num_receive = request.form['num_give']
-    db.board.delete_one({'num': int(num_receive)})
-    return jsonify({'msg': "삭제 완료"})
 
+# 게시물 검색
+@app.route('/board/regist', methods=["GET"])
+def search_get():
+    search_receive = request.args.get('searchInput')
+    print("검색", search_receive)
+    search_list = list(db.board.find({'$or': [{'title': {'$regex': search_receive}},
+                                              {'content': {'$regex': search_receive}},
+                                              {'nickname': {'$regex': search_receive}}]},
+                                     {'_id': False}))
+    print("검색 결과", search_list)
+    return jsonify({'searches': search_list})
+
+
+# 게시물 삭제
+@app.route('/board/regist/delete', methods=["POST"])
+def delete_card():
+    num = request.form.get('num_give')
+    pw = request.form.get('pw')
+    card = db.board.find_one({'num': int(num), 'password': pw})
+    print(card)
+    print(num, pw)
+    if card:
+        db.board.delete_one({'num': int(num)})
+        return jsonify({'msg': '삭제가 완료되었습니다.'})
+    else:
+        return jsonify({'msg': 'Card not found'}), 404
+
+
+# 소개 페이지
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 if __name__ == '__main__':
+    app.debug = True
     app.run('0.0.0.0', port=5000, debug=True)
